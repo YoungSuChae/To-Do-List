4ec883640607c62b65bd6eb14e45ac76afe02be9
idkcoder93 <afridman0793@conestogac.on.ca>
2023-12-23 20:36:00 -0500

    registration pages were deleted created login page instead

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // import Pages
 import LoginPage from "./pages/LoginPage";
-import Registration from "./pages/Registration";
 import MainPage from "./pages/mainPage";
 import Forgot from "./pages/Forgot";
 
@@ -14,7 +13,7 @@ function App() {
     <React.StrictMode>
       <Router>
           <Routes>
-            <Route exact path="/" element={<Registration />} />
+            <Route exact path="/" element={<LoginPage />} />
             <Route exact path="/main" element={<MainPage />} />
           </Routes>
       </Router>
