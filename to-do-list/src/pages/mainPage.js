dae7224af4cd7bbce9e2f1a8897c4cc4aa351241
idkcoder93 <afridman0793@conestogac.on.ca>
2023-12-27 13:20:12 -0500

    took away calendar asnd added modal

--- to-do-list/src/pages/mainPage.js
+++ to-do-list/src/pages/mainPage.js
@@ -1,67 +1,116 @@
-import React, { useState } from 'react';
-import Header from '../components/header';
+import React, { useState } from "react";
+import Header from "../components/header";
 import "./mainPageStyle.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BiTrash, BiCheck } from 'react-icons/bi';
-import { Button } from 'bootstrap';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BiTrash, BiCheck } from "react-icons/bi";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
 
 const MainPage = () => {
+  const [show, setShow] = useState(false);
 
-    const [value, onChange] = useState(new Date());
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-   return (
+  return (
     <React.StrictMode>
-   <Header title='Plan Your Day'></Header>
-     <div style={{textAlign: 'left', clear: 'left',marginBottom: '25px', marginTop: '10px'}}>         
-         <h1>Today's Tasks</h1>
-     </div>
-     <div className="d-flex flex-column">
-        {/** below is the example of how the tasks will look */}
-        <ul className='tasks'>
-            <li>Task 1: Description, Name, Date, Priority
-                <div className='icone-wrapper'>
-                    <BiCheck className="check"></BiCheck>
-                    <BiTrash className="trash"></BiTrash>
-                </div>
-            </li>
-            <li style={{backgroundColor: 'lightcoral'}}>Task 2: Description, Name, Date, Priority
-                <div className='icone-wrapper'>
-                    <BiCheck className="check"></BiCheck>
-                    <BiTrash className="trash"></BiTrash>
-                </div>
-            </li>
+      <Header title="Plan Your Day" />
+      <div
+        style={{
+          textAlign: "left",
+          clear: "left",
+          marginBottom: "25px",
+          marginTop: "10px",
+        }}
+      >
+        <h1>Today's Tasks</h1>
+      </div>
+      <div className="d-flex flex-column">
+        <ul className="tasks">
+          <li>
+            Task 1: Description, Name, Date, Priority
+            <div className="icone-wrapper">
+              <BiCheck className="check"></BiCheck>
+              <BiTrash className="trash"></BiTrash>
+            </div>
+          </li>
+          <li style={{ backgroundColor: "lightcoral" }}>
+            Task 2: Description, Name, Date, Priority
+            <div className="icone-wrapper">
+              <BiCheck className="check"></BiCheck>
+              <BiTrash className="trash"></BiTrash>
+            </div>
+          </li>
         </ul>
-        <button className="add-task" {/* onClick={} */...Button}>Add Task</button>
-     </div>
-     <div div style={{textAlign: 'left', clear: 'left',marginBottom: '25px', marginTop: '10px'}}>
+        <button className="add-task" onClick={handleShow}>
+          Add Task
+        </button>
+      </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <div className="task-window"></div>
+      <div
+        div
+        style={{
+          textAlign: "left",
+          clear: "left",
+          marginBottom: "25px",
+          marginTop: "10px",
+        }}
+      >
         <h1>This Weeks Tasks</h1>
-     </div>
-     <div className="d-flex flex-column">
-        {/** below is the example of how the tasks will look priority level i
-         * color coded to keep things simple */}
-        <ul className='tasks'>
-            <li>Task 1: Description, Name, Date, Priority
-                <div className='icone-wrapper'>
-                    <BiCheck className="check"></BiCheck>
-                    <BiTrash className="trash"></BiTrash>
-                </div>
-            </li>
-            <li style={{backgroundColor: 'lightcoral'}}>Task 2: Description, Name, Date, Priority
-                <div className='icone-wrapper'>
-                    <BiCheck className="check"></BiCheck>
-                    <BiTrash className="trash"></BiTrash>
-                </div>
-            </li>
+      </div>
+      <div className="d-flex flex-column">
+        <ul className="tasks">
+          <li>
+            Task 1: Description, Name, Date, Priority
+            <div className="icone-wrapper">
+              <BiCheck className="check"></BiCheck>
+              <BiTrash className="trash"></BiTrash>
+            </div>
+          </li>
+          <li style={{ backgroundColor: "lightcoral" }}>
+            Task 2: Description, Name, Date, Priority
+            <div className="icone-wrapper">
+              <BiCheck className="check"></BiCheck>
+              <BiTrash className="trash"></BiTrash>
+            </div>
+          </li>
         </ul>
-    </div>
-    <div className='calender-container'>
-        <Calendar onChange={onChange} value={value} />
-    </div>
-   </React.StrictMode>
-   )
-}
+      </div>
+    </React.StrictMode>
+  );
+};
 
 export default MainPage;
